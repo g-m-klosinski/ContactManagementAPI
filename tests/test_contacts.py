3fde0760ada626f2e83b9b5c68e15e2bb313b84c
Gregory Klosinski <g.m.klosinski.wut@gmail.com>
2026-07-27 23:48:01 +0200

    Add failing test for reading a contact

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1,11 +1,12 @@
 from httpx2 import Response
 
+
 def test_get_all(populated_client):
     response: Response = populated_client.get("/contacts")
 
     assert response.status_code == 200
 
-    data =  response.json()
+    data = response.json()
 
     assert isinstance(data, list)
 
@@ -13,7 +14,12 @@ def test_get_all(populated_client):
     assert len(data[0]) == 3
 
 
+def test_get_contact(populated_client):
+    response: Response = populated_client.get("/contacts/2")
+
+    assert response.status_code == 200
+
 
 def test_create_contact(client, sample_contact):
-    response = client.post("/contacts", json=sample_contact)
+    response: Response = client.post("/contacts", json=sample_contact)
     assert response.status_code == 201
